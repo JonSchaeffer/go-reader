ab76c5a791909da49895adbb8a6b109017bbe38f
Jonathan Schaeffer <jschaeffer97@gmail.com>
2025-07-11 23:46:23 -0400

    set allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 3000,
 		strictPort: true,
+		allowedHosts: ['reader-frontend.porgy-monitor.ts.net'], // Allow all hosts for development
 		hmr: {
 			clientPort: 3000
 		}
